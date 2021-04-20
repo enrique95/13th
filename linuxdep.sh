df6e7f932323689402062ea482f1cbd1cc8654d5
enrique95 <jenriquegg95@gmail.com>
2021-04-20 14:16:11 +0200

    removed confirmation message

--- linuxdep.sh
+++ linuxdep.sh
@@ -8,15 +8,15 @@
 # developing tools for building packages in case you don't have them in 
 # your minimalist distro
 
-sudo pacman -S base-devel gcc-fortran
+sudo pacman -S --noconfirm base-devel gcc-fortran yay
 
 # in order to install the `units` package and dependency of `sf`
 # you need to install this library from the AUR, not available in Arch repo
 
-yay -S udunits
+yay -S --noconfirm udunits
 
 # libraries needed to install `tesseract` and `sf` packages
 # only the english dictionary is needed for tesseract
 
-sudo pacman -S base-devel tesseract tesseract-data-eng gdal gcc-fortran
+sudo pacman -S --noconfirm base-devel tesseract tesseract-data-eng gdal gcc-fortran
 
